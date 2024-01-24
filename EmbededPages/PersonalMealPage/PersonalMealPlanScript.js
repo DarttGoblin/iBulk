0a47eafa80c3a491d74ce9244ad19403fd1e3517
DarttGoblin <112183975+DarttGoblin@users.noreply.github.com>
2024-01-24 16:15:40 +0100

    Delete EmbededPages directory

--- EmbededPages/PersonalMealPage/PersonalMealPlanScript.js
+++
@@ -1,9 +0,0 @@
-//  Recieving The Calories Amount To Personal Meal Plan
-var amountOfCalories = Number(localStorage.getItem("amountOfCalories"));
-var alertIsActivated = JSON.parse(localStorage.getItem("alertIsActivated"));
-if (alertIsActivated) {
-    alertMessage = "Your amount of calories is " + amountOfCalories + ". No suggested meal for this amount. please try again later";
-    setTimeout(() => {alert(alertMessage);}, 2000);
-    alertIsActivated = false;
-    localStorage.setItem("alertIsActivated", alertIsActivated);
-}
